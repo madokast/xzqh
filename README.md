0dae0f58c4e87a3403569b7993da4168090b6f42
zhaorunxiao <zhaorunxiao@sics.ac.cn>
2024-06-19 09:47:37 +0800

    resolve_history_code

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 
 - resolve_code(code) 解析最新（2023年）的行政区划
 - resolve_code(code， year) 解析指定年份的行政区划
+- resolve_history_code(code) 解析行政区划代码，在每年的意义
 
 ```
 import xzqh
 print(xzqh.resolve_code(110101)) # ('北京市', '', '东城区')
 print(xzqh.resolve_code(110201)) # ('', '', '')
 print(xzqh.resolve_code(110201, 1980)) # ('北京市', '', '昌平县')
+
+print(xzqh.resolve_history_code(110201)) # {1980: ('北京市', '', '昌平县'), 1981: ('北京市', '', '昌平县')}
 ```
 
--- xzqh.py
+++ xzqh.py
@@ -1,7 +1,8 @@
 
 import json
 _Data = None
-_curYear = "2023"
+_curYear = 2023
+_startYear = 1980
 
 def resolve_code(code, year=None):
     global _Data
@@ -12,10 +13,20 @@ def resolve_code(code, year=None):
     if years is None:
         return ("", "", "")
     if year is None:
-        year = _curYear
+        year = str(_curYear)
     return tuple(years.get(str(year), ("", "", "")))
+
+def resolve_history_code(code):
+    years = dict()
+    for year in range(_startYear, _curYear+1):
+        coded = resolve_code(code, year)
+        if "".join(coded) != "":
+            years[year] = coded
+    return years
 
 if __name__ == '__main__':
     print(resolve_code(110101)) # ('北京市', '', '东城区')
     print(resolve_code(110201)) # ('', '', '')
     print(resolve_code(110201, 1980)) # ('北京市', '', '昌平县')
+
+    print(resolve_history_code(110201)) # {1980: ('北京市', '', '昌平县'), 1981: ('北京市', '', '昌平县')}
